c750ae6ec215afd70cf5862e58b9a02b37b2db89
Abdel-moniem E. REZK <arezk@live.co.uk>
2018-06-05 11:52:37 +0100

    Lists and Tuples together

---
+++ read_write_exercise_PRNE/List_tuple.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+from collections import namedtuple
+
+devices=[]
+
+#opening the file
+
+newfile= open('list.csv', 'r')
+
+for line in newfile:
+    device_info = tuple(line.strip().split(','))
+    print 'Devices info: ', device_info
+    devices.append(device_info)
+
+pprint(devices)
+
+newfile.close()
+
+
